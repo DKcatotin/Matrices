89bb04e052db2475076a62c2cbc4c4631c32d6c2
JDcatota <jda.catota@yavirac.edu.ec>
2024-11-28 22:08:03 -0500

    tarea 1 y hasta 3 echa

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,6 +2,5 @@ import { Routes } from '@angular/router';
 
 export const routes: Routes = [
     {path: 'deberes',loadChildren:()=>import('./deberes/deberes.module').then(deb=>deb.DeberesModule)},
-    
     {path: '', redirectTo: '/deberes', pathMatch: 'full' },
 ];
