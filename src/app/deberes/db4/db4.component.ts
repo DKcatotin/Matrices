5ecd3505c75c06728c687e0305598c20a1939e5b
JDcatota <jda.catota@yavirac.edu.ec>
2024-12-09 22:03:05 -0500

    formulario mas boton

--- src/app/deberes/db4/db4.component.ts
+++ src/app/deberes/db4/db4.component.ts
@@ -8,5 +8,24 @@ import { Component } from '@angular/core';
   styleUrl: './db4.component.css'
 })
 export class Db4Component {
+  visibleForm: number | null = null; 
+  form1Data = { name: '' };
+  form2Data = { email: '' };
+  form3Data = { phone: '' };
 
+  toggleForm(formNumber: number) {
+    this.visibleForm = this.visibleForm === formNumber ? null : formNumber;
+  }
+
+  onSubmit(formName: string) {
+    console.log(`${formName} enviado:`);
+    if (formName === 'Formulario 1') {
+      console.log(this.form1Data);
+    } else if (formName === 'Formulario 2') {
+      console.log(this.form2Data);
+    } else if (formName === 'Formulario 3') {
+      console.log(this.form3Data);
+    }
+  }
 }
+
--- src/app/deberes/deberes.module.ts
+++ src/app/deberes/deberes.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
-
+import { InputTextModule } from 'primeng/inputtext';
 import { DeberesRoutingModule } from './deberes-routing.module';
 import { Db1Component } from './db1/db1.component';
 import { Db2Component } from './db2/db2.component';
@@ -12,6 +12,7 @@ import { LayoutComponent } from './layout/layout.component';
 
 import { CardModule } from 'primeng/card';
 import { ButtonModule } from 'primeng/button';
+import { FormsModule } from '@angular/forms';
 
 
 @NgModule({
@@ -29,6 +30,8 @@ import { ButtonModule } from 'primeng/button';
     RouterModule,
     CardModule,
     ButtonModule,
+    InputTextModule,
+    FormsModule,
   ]
 })
 export class DeberesModule { }
