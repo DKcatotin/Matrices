6e54e368d47667293512f99b922ff1c722af7ae6
JDcatota <jda.catota@yavirac.edu.ec>
2024-12-15 12:52:57 -0500

    cambio en cards

--- src/app/deberes/db3/db3.component.ts
+++ src/app/deberes/db3/db3.component.ts
@@ -8,5 +8,80 @@ import { Component } from '@angular/core';
   styleUrl: './db3.component.css'
 })
 export class Db3Component {
-
+  cards = [
+    {
+      header: 'Card 1',
+      subheader: 'Subheader 1',
+      image: 'https://lp-cms-production.imgix.net/2019-06/77186995.jpg?fit=crop&q=40&sharp=10&vib=20&auto=format&ixlib=react-8.6.4',
+      description: 'Mira qué hermoso paisaje 1'
+    },
+    {
+      header: 'Card 2',
+      subheader: 'Subheader 2',
+      image: 'https://lp-cms-production.imgix.net/2019-06/77186995.jpg?fit=crop&q=40&sharp=10&vib=20&auto=format&ixlib=react-8.6.4',
+      description: 'Mira qué hermoso paisaje 2'
+    },
+    {
+      header: 'Card 3',
+      subheader: 'Subheader 3',
+      image: 'https://lp-cms-production.imgix.net/2019-06/77186995.jpg?fit=crop&q=40&sharp=10&vib=20&auto=format&ixlib=react-8.6.4',
+      description: 'Mira qué hermoso paisaje 4'
+    },{
+      header: 'Card 4',
+      subheader: 'Subheader 4',
+      image: 'https://lp-cms-production.imgix.net/2019-06/77186995.jpg?fit=crop&q=40&sharp=10&vib=20&auto=format&ixlib=react-8.6.4',
+      description: 'Mira qué hermoso paisaje 2'
+    },{
+      header: 'Card 5',
+      subheader: 'Subheader 5',
+      image: 'https://lp-cms-production.imgix.net/2019-06/77186995.jpg?fit=crop&q=40&sharp=10&vib=20&auto=format&ixlib=react-8.6.4',
+      description: 'Mira qué hermoso paisaje 2'
+    },{
+      header: 'Card 6',
+      subheader: 'Subheader 6',
+      image: 'https://lp-cms-production.imgix.net/2019-06/77186995.jpg?fit=crop&q=40&sharp=10&vib=20&auto=format&ixlib=react-8.6.4',
+      description: 'Mira qué hermoso paisaje 2'
+    },{
+      header: 'Card 7',
+      subheader: 'Subheader 7',
+      image: 'https://lp-cms-production.imgix.net/2019-06/77186995.jpg?fit=crop&q=40&sharp=10&vib=20&auto=format&ixlib=react-8.6.4',
+      description: 'Mira qué hermoso paisaje 2'
+    },{
+      header: 'Card 8',
+      subheader: 'Subheader 8',
+      image: 'https://lp-cms-production.imgix.net/2019-06/77186995.jpg?fit=crop&q=40&sharp=10&vib=20&auto=format&ixlib=react-8.6.4',
+      description: 'Mira qué hermoso paisaje 2'
+    },{
+      header: 'Card 9',
+      subheader: 'Subheader 9',
+      image: 'https://lp-cms-production.imgix.net/2019-06/77186995.jpg?fit=crop&q=40&sharp=10&vib=20&auto=format&ixlib=react-8.6.4',
+      description: 'Mira qué hermoso paisaje 2'
+    },{
+      header: 'Card 10',
+      subheader: 'Subheader 10',
+      image: 'https://lp-cms-production.imgix.net/2019-06/77186995.jpg?fit=crop&q=40&sharp=10&vib=20&auto=format&ixlib=react-8.6.4',
+      description: 'Mira qué hermoso paisaje 2'
+    },{
+      header: 'Card 11',
+      subheader: 'Subheader 11',
+      image: 'https://lp-cms-production.imgix.net/2019-06/77186995.jpg?fit=crop&q=40&sharp=10&vib=20&auto=format&ixlib=react-8.6.4',
+      description: 'Mira qué hermoso paisaje 2'
+    },{
+      header: 'Card 11',
+      subheader: 'Subheader 11',
+      image: 'https://lp-cms-production.imgix.net/2019-06/77186995.jpg?fit=crop&q=40&sharp=10&vib=20&auto=format&ixlib=react-8.6.4',
+      description: 'Mira qué hermoso paisaje 2'
+    },{
+      header: 'Card 11',
+      subheader: 'Subheader 1',
+      image: 'https://lp-cms-production.imgix.net/2019-06/77186995.jpg?fit=crop&q=40&sharp=10&vib=20&auto=format&ixlib=react-8.6.4',
+      description: 'Mira qué hermoso paisaje 11'
+    },{
+      header: 'Card 12',
+      subheader: 'Subheader 12',
+      image: 'https://lp-cms-production.imgix.net/2019-06/77186995.jpg?fit=crop&q=40&sharp=10&vib=20&auto=format&ixlib=react-8.6.4',
+      description: 'Mira qué hermoso paisaje 12'
+    }
+  ];
 }
+
